03df812ca14561f56cd092e14e31f45143a1d64e
sesahe <sesahe@alumnatflorida.es>
2024-11-04 17:41:07 +0100

    Se añaden palabras a la lista del juego en index.php

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Lista de palabras para el juego
-$palabras = ['elefante', 'jirafa', 'hipopotamo', 'rinoceronte', 'cocodrilo', 'camello', 'chimpance'];
+$palabras = ['elefante', 'jirafa', 'hipopotamo', 'rinoceronte', 'cocodrilo', 'camello', 'chimpance', 'tigre', 'león', 'jaguar'];
 
 // Inicializar el juego
 if (!isset($_SESSION['palabra'])) {
